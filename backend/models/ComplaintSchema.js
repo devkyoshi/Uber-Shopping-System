b61ab8f2fb3658320151abf65f44fa463d6475ed
IT22107978 <154581144+ChaLinP@users.noreply.github.com>
2024-03-19 09:12:17 +0530

    ComplaintSchema
    By chanmi

---
+++ backend/models/ComplaintSchema.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+
+const ComplaintSchema = new mongoose.Schema({
+    complaint_ID:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:10
+    },
+    complaint_type:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    },
+    Order_ID:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:10
+    },
+    Resolving_option:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:20
+    },
+     Payment_ID:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:10
+    },
+    /*Complaint_image:{
+        type:Buffer,  
+        required:true, 
+    },*/
+    Quantity:{
+        type:Number,
+        required:true,
+        trim:true
+    },
+    Comlaint_Status:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:10
+    }
+
+},{timestamps:true})
+
+module.exports = mongoose.model('Complaint',ComplaintSchema)
