22cfcba195ad13eddeed03980abb29db4e3c32d8
IT22107978 <154581144+ChaLinP@users.noreply.github.com>
2024-03-19 09:59:46 +0530

    comment simple code

--- backend/server.js
+++ backend/server.js
@@ -49,3 +49,35 @@ app.use("/customer", customerRouter);
 app.listen(PORT, () =>{
     console.log(`Server is up and running no port:  ${PORT}`)
 });
+
+
+/*
+//Look how simple the code is
+
+const express = require('express')
+const cors = require('cors')
+const {db} = require('./db/db')
+//for routing
+const {readdirSync} = require('fs')
+const app = express()
+
+require('dotenv').config()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1',require('./routes/' + route)))
+
+const PORT = process.env.PORT
+
+const server = () =>{
+    db()
+    app.listen(PORT,() =>{
+        console.log('listening to port : ',PORT)
+    })
+
+}
+
+server()*/
