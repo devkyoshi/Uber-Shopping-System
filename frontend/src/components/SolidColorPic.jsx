3be799b5a141f457855d2147a31ad3bc91bc2976
IT22107978 <154581144+ChaLinP@users.noreply.github.com>
2024-04-06 11:36:01 +0530

    About

---
+++ frontend/src/components/SolidColorPic.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export function SolidColorPic(){
+    return(
+        <div className='solid-color-pic'>
+            
+
+        </div>
+    )
+}
+
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { SolidColorPic } from '../components/SolidColorPic'
+
 
 export default function About() {
   return (
     <div className='inner-layout'>
-      About
+      <SolidColorPic/>
     </div>
   )
 }
