8cbd3fd7a52c80b7d9a31fd9f4e60bad2f63f646
IT22107978 <154581144+ChaLinP@users.noreply.github.com>
2024-04-06 18:27:43 +0530

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,8 +10,9 @@ export default function About() {
       <p className='share'>To share love through grocery deliveries</p>
       <h3 className='ourM'>Quality Products,Delivered with Care</h3>
       <p className='share'>We understand that life can be busy,and finding time to shop for groceries can be a challege.
-      That's where we come in.Our team works tirelessly to handpick the freshest and highest-quality products for you.From farm-fresh produce to pantry 
-      staples and gourment treats,we've got everything you need to create delicious meals and memorable moments at home.</p>
+      That's where we come in.Our team works tirelessly to handpick the freshest and highest-quality products for you.From 
+      farm-fresh produce to pantry staples and gourment treats,we've got everything you need to create delicious meals and 
+      memorable moments at home.</p>
       <h3 className='ourM'>Exeptional Service,Every Step of the Way</h3>
       <p className='share'>When you shop with Uber.Shopping you can expect more than just groceries.
       You can expect exceptional service from start to finish.
@@ -24,4 +25,5 @@ export default function About() {
       trusted partner in nourising your family and spread love, one delivery at a time.</p>
     </div>
   )
+
 }
