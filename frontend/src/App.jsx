fae315488a44f4dd54905cfff1e8f35216612c1d
Ashan Tharindu <153891101+DevAshiZ@users.noreply.github.com>
2024-04-06 08:54:21 +0530

    Nav component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,17 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
-import { SideBar } from "./components/SideBar";
-import { Footer } from "./components/Footer";
-import { Promotions } from "./components/Promotions";
 import OrderForm from "./pages/OrderForm";
+import HomeUnregistered from "./pages/HomeUnregistered";
+import { NavigationBar } from "./components/NavigationBar";
 
 export default function App() {
   return (
     <BrowserRouter >
-    {/* <Header/> */}
-    {/* <Navigation/> */}
-    {/* <Promotions/>
-    <SideBar/> */}
+    <NavigationBar/>
     <Routes>
+      
+      <Route path="/unregiHome" element = {<HomeUnregistered/>}/>
       <Route path="/home" element = {<Home/>}/>
       <Route path="/signin" element = {<SignIn/>}/>
       <Route path="/signup" element = {<SignUp/>}/>
