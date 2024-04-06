501d6c8a1ea15ab02879304d3a8e8012aa44a6c6
IT22107978 <154581144+ChaLinP@users.noreply.github.com>
2024-04-06 12:57:23 +0530

    SolidColorPic.jsx

--- frontend/src/components/SolidColorPic.jsx
+++ frontend/src/components/SolidColorPic.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import delivery from '../img/delivery.jpg'
 
 export function SolidColorPic(){
     return(
         <div className='solid-color-pic'>
-            
-
+            <img src={delivery} alt="delivery" className='delivery-img'></img>
+            <div className='text-middle'>
+                <h2>About Uber Shopping</h2>
+                <p>A convenient way to shop for groceries</p>
+            </div>
         </div>
     )
 }
