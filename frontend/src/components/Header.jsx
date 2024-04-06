ef0b4ca16f1f9fde3887ec50b982eb437ff2f80b
yashodalasith <yashodhalasithjayasinghe@gmail.com>
2024-04-06 20:24:31 +0530

    frontend and backend for customer register

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,86 @@
-import {FaSearch} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
+// import { Avatar, Dropdown, Navbar, TextInput } from 'flowbite-react';
+// import React from 'react';
+// import { Link, useLocation } from 'react-router-dom';
+// import { Button } from 'flowbite-react';
+// import { AiOutlineSearch } from 'react-icons/ai';
+// //import { FaMoon } from 'react-icons/fa'
+// import { useSelector } from 'react-redux';
+
+// export default function Header() {
+//     const path = useLocation().pathname;
+//     const { currentCustomer } = useSelector((state) => state.customer);
+//   return (
+//     <Navbar className='border-b-2'>
+//         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+//             <span className='px-2 py-1 bg-gradient-to-r from-pink-500 via-red-500 to-orange-500 rounded-lg text-white'>Food</span>Delivery
+//         </Link>
+//         <form>
+//             <TextInput 
+//                 type='text'
+//                 placeholder='Search..'
+//                 rightIcon={AiOutlineSearch}
+//                 className='hidden lg:inline'
+//             />
+//         </form>
+//         <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+//             <AiOutlineSearch />
+//         </Button>
+//         <div className='flex gap-2 md:order-2'>
+//             {/*<Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+//                 <FaMoon />
+//             </Button>*/}
+//             {currentCustomer ? (
+//                 <Dropdown arrowIcon={false} inline 
+//                 label={<Avatar
+//                     alt= 'user'
+//                     img = ''
+//                     rounded
+//                 />}>
+//                     <Dropdown.Header>
+//                         <span classname='block text-sm'>Username: {currentCustomer.Cus_Username}</span><br></br>
+//                         <span classname='block text-sm font-medium truncate'>E-mail: {currentCustomer.Cus_Email}</span>
+//                     </Dropdown.Header>
+//                     <Link to={'/profile?tab=profile'}>
+//                         <Dropdown.Item>Profile</Dropdown.Item>
+//                     </Link>
+//                     <Dropdown.Divider/>
+//                     <Dropdown.Item>Sign out</Dropdown.Item>
+//                 </Dropdown>
+
+//             ): (
+//                 <Link to='/login'>
+//                     <Button gradientDuoTone='pinkToOrange' outline>
+//                         Log in
+//                     </Button>
+//                 </Link>
+//             )}
+//             {/*<Link to='/login'>
+//                 <Button gradientDuoTone='pinkToOrange' outline>
+//                     Log in
+//                 </Button>
+//             </Link>*/}
+//             <Navbar.Toggle></Navbar.Toggle>
+//         </div>
+//         <Navbar.Collapse>
+//                 <Navbar.Link active={path === '/home'} as={'div'}>
+//                     <Link to='/home'>Home</Link>
+//                 </Navbar.Link>
+//                 <Navbar.Link active={path === '/about'} as={'div'}>
+//                     <Link to='/about'>About</Link>
+//                 </Navbar.Link>
+//                 <Navbar.Link active={path === '/project'} as={'div'}>
+//                     <Link to='/project'>Project</Link>
+//                 </Navbar.Link>
+//             </Navbar.Collapse>
+//     </Navbar>
+//   )
+// }
+import React from 'react'
 
 export default function Header() {
   return (
-    <header className='shadow-md header-bg'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <Link to = '/'>
-                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'> 
-                    <span className=' logo-span1'>Uber</span>
-                    <span className=' logo-span2'>Shopping</span>
-                </h1>
-            </Link>
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-                <FaSearch className='text-slate-500 '/>
-            </form>
-            <ul className='flex gap-4'>
-                <Link to='/home'><li className='hidden sm:inline text-main font-bold hover:underline'>Home</li></Link>
-                <Link to='/about'> <li className='hidden sm:inline text-main font-bold hover:underline'>About</li></Link>
-                <Link to='/signin'><li className='text-main font-bold hover:underline'>Sign In</li></Link>
-            </ul>
-        </div>
-    </header>
+    <div>
+      header
+    </div>
   )
 }
