c8da48239137f3ea7bfe4b6c419f22c93219e730
IT22107978 <154581144+ChaLinP@users.noreply.github.com>
2024-04-06 13:08:31 +0530

    Update SolidColorPic.jsx

--- frontend/src/components/SolidColorPic.jsx
+++ frontend/src/components/SolidColorPic.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import delivery from '../img/delivery.jpg'
+import service from '../img/service.jpg'
 
 export function SolidColorPic(){
     return(
         <div className='solid-color-pic'>
             <img src={delivery} alt="delivery" className='delivery-img'></img>
+            <img src={service} alt="delivery" className='service-img'></img>
             <div className='text-middle'>
                 <h2>About Uber Shopping</h2>
                 <p>A convenient way to shop for groceries</p>
