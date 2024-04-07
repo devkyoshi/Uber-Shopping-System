38c6eb4aa286fb9de858379f736fcc7f01a6c71c
IT22107978 <154581144+ChaLinP@users.noreply.github.com>
2024-04-07 14:00:05 +0530

    Update Complaint.jsx

--- frontend/src/pages/Complaint.jsx
+++ frontend/src/pages/Complaint.jsx
@@ -1,6 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import {useSelector} from 'react-redux'
 import axios  from 'axios'
+import {Button} from '@material-tailwind/react'
 
 export default function Complaint(){
     const userId = useSelector((state) => state.userId);
@@ -9,7 +10,12 @@ export default function Complaint(){
     
     return(
         <div className='inner-layout'>
-         Complaint
+            <div className='bg-gray-700 text-white py-4 px-6 rounded-lg shadow-md'>
+                <h3 className='text-xl font-semibold'>Complaints</h3>
+            </div>
+            <div className='flex justify-end mt-4'>
+            <Button color='gray' ripple="light">New Complaint</Button>
+            </div>
         </div>
     )
 }
