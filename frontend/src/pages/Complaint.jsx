38b80c8b159412dff49a2a0c7c211a3b208bd1d4
IT22107978 <154581144+ChaLinP@users.noreply.github.com>
2024-04-08 13:37:16 +0530

    Update Complaint.jsx

--- frontend/src/pages/Complaint.jsx
+++ frontend/src/pages/Complaint.jsx
@@ -1,17 +1,20 @@
 import React,{useEffect, useState} from 'react'
-import {useSelector} from 'react-redux'
 import axios  from 'axios'
 import {Button} from '@material-tailwind/react'
 
 export default function Complaint(){
-    const userId = useSelector((state) => state.userId);
+    
     const [complaints,setComplaints] = useState([]);
 
-    const previousComplaints = [
-        {id:1,order_id:'666789898989asdf',complaint_status:'Pending',item_id:'666789898989asdfe',quantity:'1.2kg'},
-        {id:2,order_id:'666789898989asdf',complaint_status:'Pending',item_id:'666789898989asdfe',quantity:'2'},
-        {id:3,order_id:'666789898989asdf',complaint_status:'Pending',item_id:'666789898989asdfe',quantity:'2.3g'}
-    ]
+    useEffect(() => {
+        axios.get('http://localhost:8070/Complaint/complaint-all')
+        .then(response => {
+            setComplaints(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:',error)
+        })
+    },[]);
 
     
     return(
@@ -24,21 +27,22 @@ export default function Complaint(){
             </div>
             <br/>
             <ul>
-                {previousComplaints.map((Complaint) => (
-                    <li key={Complaint.id} className='mb-2'>
+                {complaints.map((Complaints) => (
+                    <li key={Complaints._id} className='mb-2'>
                         <div className='border border-gray-400 bg-gray-300 p-4 rounded-lg items-center justify-between'>
                             <div className='flex'>
-                               <strong className='mr-2'>Order ID : </strong>{Complaint.order_id}
+                               <strong className='mr-2'>Order ID : </strong>{Complaints.order_id}
                                <strong className='ml-10 mr-2'>Complaint Status : </strong>
-                               <strong><div className='border border-gray-300 text-red-800 bg-white px-4 rounded-lg shadow-md'>{Complaint.complaint_status}</div></strong>
+                               <strong><div className='border border-gray-300 text-red-800 bg-white px-4 rounded-lg shadow-md'>
+                                {Complaints.complaint_status}</div></strong>
                                <div className='ml-auto flex'>
                                <Button color='blue' ripple='light' className='w-30 mr-3 ' size='regular'>Edit</Button>
                                <Button color='red' ripple='light' className='w-30' size='regular'>Delete</Button>
                                </div>
                            </div>
                            <div className='flex'>
-                               <div className='mr-3'>Item ID : </div>{Complaint.item_id}
-                               <div className='ml-10 mr-3'>Quantity : </div>{Complaint.quantity}
+                               <div className='mr-5'>Item ID :  </div>{Complaints.item_id}
+                               <div className='ml-10 mr-3'>Quantity : </div>{Complaints.quantity}
                            </div>
                         </div>
                     </li>
