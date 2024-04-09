b532ca41123e5069bbcb20231110923ae250c74b
Ashan Tharindu <153891101+DevAshiZ@users.noreply.github.com>
2024-04-09 17:28:23 +0530

    branch task thingy

--- backend/models/task.js
+++ backend/models/task.js
@@ -1,42 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const taskSchema = new Schema({
-    driver_id: {
+  driver_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Employee",
+    required: true,
+  },
+  task_status: {
+    type: String,
+    default: "ongoing",
+  },
+  route: {
+    type: String,
+  },
+  orders: [
+    {
+      order_id: {
         type: Schema.Types.ObjectId,
-        ref: 'Employee',
-        required: true
-    },
-    task_status: {
+        ref: "Order",
+        required: true,
+      },
+      status: {
         type: String,
-        default: 'ongoing'
-    },
-    route: {
+      },
+      pickup_image_url: {
         type: String,
+      },
+      pickedup_time: {
+        type: Date,
+      },
+      delivered_image_url: {
+        type: String,
+      },
+      delivered_time: {
+        type: Date,
+      },
     },
-    orders: [{
-        order_id: {
-            type: Schema.Types.ObjectId,
-            ref: 'Order',
-            required: true
-        },
-        status: {
-            type: String,
-        },
-        pickup_image_url: {
-            type: String,
-        },
-        delivered_image_url: {
-            type: String,
-        },
-        delivered_time:{
-            type: Date,
-        },
-        pickedup_time: {
-            type: Date,
-        }
-    }]
+  ],
 });
 
-const Task = mongoose.model('Task', taskSchema);
-module.exports = Task;
+const Task = mongoose.model("Task", taskSchema);
+module.exports = Task;
