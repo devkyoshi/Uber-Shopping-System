80a183b393ab29e77c5817370afa0c805d8c48b0
Ashan Tharindu <153891101+DevAshiZ@users.noreply.github.com>
2024-04-30 06:55:43 +0530

    Update PromotionContainer.jsx

--- frontend/src/components/home/PromotionContainer.jsx
+++ frontend/src/components/home/PromotionContainer.jsx
@@ -5,19 +5,19 @@ export function PromotionContainer() {
     <div className="px-10 py-8">
       <Carousel loop={true} autoplay={true} className="rounded-xl">
         <img
-          src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          src="https://downloadpsd.cc/wp-content/uploads/2020/07/Free-Discount-Coupons-Template-PSD.jpg"
           alt="image 1"
           className="object-cover object-center"
           style={{ height: "50vh", width: "100%" }}
         />
         <img
-          src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+          src="https://freedesignfile.com/upload/2019/01/Sale-Discount-Coupon-PSD-Template.jpg"
           alt="image 2"
           className="object-cover object-center"
           style={{ height: "50vh", width: "100%" }}
         />
         <img
-          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+          src="https://static.vecteezy.com/system/resources/previews/000/179/076/original/vector-biggest-sale-offers-and-discount-banner-template-for-promotion.jpg"
           alt="image 3"
           className="object-cover object-center"
           style={{ height: "50vh", width: "100%" }}
