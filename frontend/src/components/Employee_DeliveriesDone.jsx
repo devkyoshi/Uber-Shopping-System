47c1bde2e6c56223244424f4dbe297e9bbe42ffa
Ashan Tharindu <153891101+DevAshiZ@users.noreply.github.com>
2024-05-12 12:08:18 +0530

    driverUI fixes

--- frontend/src/components/Employee_DeliveriesDone.jsx
+++ frontend/src/components/Employee_DeliveriesDone.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TaskTable from "./taskdriver/taskdriverTable";
 import { useSelector } from "react-redux";
+import { TaskDriverTab } from "./taskdriver/taskdriverTab";
 
 export default function Employee_DeliveriesDone() {
   const { currentUser } = useSelector((state) => state.user);
@@ -8,7 +9,7 @@ export default function Employee_DeliveriesDone() {
   console.log("Current Employee: ", currentUser._id);
   return (
     <div className="inner-layout mt-5 ml-2 mr-5">
-      <TaskTable user_id={currentUser._id} />
+      <TaskDriverTab user_id={currentUser._id} />
     </div>
   );
 }
--- frontend/src/components/taskdriver/taskdriverTab.jsx
+++ frontend/src/components/taskdriver/taskdriverTab.jsx
@@ -6,37 +6,29 @@ import {
   Tab,
   TabPanel,
 } from "@material-tailwind/react";
-import {
-  PlusCircleIcon,
-  EyeIcon
-} from "@heroicons/react/24/solid";
+import { PlusCircleIcon, EyeIcon } from "@heroicons/react/24/solid";
 import AddTaskDriverUI from "../../pages/taskdriver/AddTaskDriverUI";
-import ViewTaskDriverUI from "../../pages/taskdriver/ViewTaskDriverUI";
+import DriverComplaintForm from "../../pages/taskdriver/ViewTaskDriverUI";
 
- 
-export function TaskDriverTab() {
-const [activeTab,setactiveTab]=useState("add");
+export function TaskDriverTab(user_id) {
+  const [activeTab, setactiveTab] = useState("add");
 
   const tapMapping = {
-    add: <AddTaskDriverUI />,
-    view: <ViewTaskDriverUI/>
+    add: <AddTaskDriverUI user_id={user_id} />,
+    view: <DriverComplaintForm />,
   };
-
 
   const data = [
     {
       label: "Task Progress",
       value: "add",
       icon: PlusCircleIcon,
-     
     },
     {
       label: "My Information",
       value: "view",
       icon: EyeIcon,
-    
     },
-   
   ];
 
   const handleTab = (value) => {
@@ -47,7 +39,7 @@ const [activeTab,setactiveTab]=useState("add");
     <Tabs value={activeTab}>
       <TabsHeader>
         {data.map(({ label, value, icon }) => (
-         <Tab key={value} value={value} onClick={() => handleTab(value)}>
+          <Tab key={value} value={value} onClick={() => handleTab(value)}>
             <div className="flex items-center gap-2">
               {React.createElement(icon, { className: "w-5 h-5" })}
               {label}
