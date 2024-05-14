38e5ba5a6dff1ed80c78c13b62c82a2d61409580
samudithamathota <154769908+samudithamathota@users.noreply.github.com>
2024-05-14 08:19:47 +0530

    make some changes

--- frontend/src/components/task/Delivery.jsx
+++ frontend/src/components/task/Delivery.jsx
@@ -30,6 +30,16 @@ export function Delivery() {
   };
 
   const handleUpdate = async (updateddeliver) => {
+    // Check if any field is null
+    if (
+      updateddeliver.chargePrice === null ||
+      updateddeliver.deliveryFree === null ||
+      updateddeliver.interest === null
+    ) {
+      window.alert("One or more fields are null. Please fill them out.");
+      return; // Do nothing if any field is null
+    }
+
     try {
       await axios.put(
         `http://localhost:8070/Delivery/delivery-update/${updateddeliver._id}`,
@@ -49,7 +59,7 @@ export function Delivery() {
       if (deliver._id === deliverId) {
         return {
           ...deliver,
-          [fieldName]: value,
+          [fieldName]: value === "" ? null : value,
         };
       }
       return deliver;
@@ -108,13 +118,9 @@ export function Delivery() {
   };
 
   const handleAddDelivery = async () => {
+    // Check if any of the fields are null
     try {
-      const response = await axios.post(
-        "http://localhost:8070/Delivery/delivery-add",
-        formData
-      );
-      const newDelivery = response.data;
-      setDelivers([...delivers, newDelivery]);
+      await axios.post("http://localhost:8070/Delivery/delivery-add", formData);
       setShowAddForm(false);
       setFormData({
         chargePrice: "",
@@ -123,20 +129,112 @@ export function Delivery() {
       });
     } catch (error) {
       console.error("Error adding delivery:", error);
-      setErrorMessage("Error adding delivery");
+      window.alert("Please Fill the Fields");
     }
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+  const handleNumericInputChange = (e, id, field) => {
+    const value = e.target.value;
+    // Check if the value is numeric
+    if (!/[^1-9]/g.test(value)) {
+      // If numeric, update the state
+      handleInputChange(e, id, field);
+    }
   };
 
   return (
     <Card className="h-full w-full">
+      {delivers.length > 0 && (
+        <table className="w-full min-w-min table-auto text-left">
+          <thead>
+            <tr>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 pl-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  style={{ fontWeight: "bolder" }}
+                  className="font-normal leading-none opacity-70 text-center"
+                >
+                  Normal delivery charge
+                </Typography>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 pl-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  style={{ fontWeight: "bolder" }}
+                  className="font-normal leading-none opacity-70 text-center"
+                >
+                  Delivery Free Threshold
+                </Typography>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 pl-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  style={{ fontWeight: "bolder" }}
+                  className="font-normal leading-none opacity-70 text-center"
+                >
+                  Percentage
+                </Typography>
+              </th>
+
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50   pt-4 pb-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  style={{ fontWeight: "bolder" }}
+                  className="font-normal leading-none opacity-70 text-center"
+                >
+                  Update
+                </Typography>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {delivers.map((deliver) => (
+              <tr key={deliver._id} className="mb-4">
+                <td className="pl-12">
+                  <Input
+                    type="text"
+                    value={deliver.chargePrice}
+                    onChange={(e) =>
+                      handleNumericInputChange(e, deliver._id, "chargePrice")
+                    }
+                    disabled={!deliver.editMode}
+                    required
+                  />
+                </td>
+                <td className="pl-6">
+                  <Input
+                    type="text"
+                    value={deliver.deliveryFree}
+                    onChange={(e) =>
+                      handleNumericInputChange(e, deliver._id, "deliveryFree")
+                    }
+                    disabled={!deliver.editMode}
+                    required
+                  />
+                </td>
+                <td className="pl-6">
+                  <Input
+                    type="text"
+                    value={deliver.interest}
+                    onChange={(e) =>
+                      handleNumericInputChange(e, deliver._id, "interest")
+                    }
+                    disabled={!deliver.editMode}
+                    required
+                  />
+                </td>
+                <td className="pl-20 pb-4 pt-4">
+                  {renderSaveOrEditButton(deliver._id)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
       {delivers.length === 0 && !showAddForm && (
         <div>
           <div className="grid grid-cols-2 gap-8 ">
@@ -149,7 +247,7 @@ export function Delivery() {
                 value={formData.chargePrice}
                 onChange={(e) => {
                   const { value } = e.target;
-                  const filteredValue = value.replace(/[^0-9]/g, "");
+                  const filteredValue = value.replace(/[^1-9]/g, "");
                   setFormData({
                     ...formData,
                     chargePrice: filteredValue,
@@ -169,7 +267,7 @@ export function Delivery() {
                 value={formData.deliveryFree}
                 onChange={(e) => {
                   const { value } = e.target;
-                  const filteredValue = value.replace(/[^0-9]/g, "");
+                  const filteredValue = value.replace(/[^1-9]/g, "");
                   setFormData({
                     ...formData,
                     deliveryFree: filteredValue,
@@ -189,7 +287,7 @@ export function Delivery() {
                   value={formData.interest}
                   onChange={(e) => {
                     const { value } = e.target;
-                    const filteredValue = value.replace(/[^0-9]/g, "");
+                    const filteredValue = value.replace(/[^1-9]/g, "");
                     setFormData({
                       ...formData,
                       interest: filteredValue,
@@ -205,92 +303,55 @@ export function Delivery() {
           <Button onClick={handleAddDelivery}>Add Delivery</Button>
         </div>
       )}
-      {delivers.length > 0 && (
-        <table className="w-full min-w-0 table-auto text-center">
-          <thead>
-            <tr>
-              <th className="border-b border-blue-gray-100 bg-blue-gray-50 ">
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  style={{ fontWeight: "bolder" }}
-                  className="font-normal leading-none opacity-70 text-center"
-                >
-                  Charge Price
-                </Typography>
-              </th>
-              <th className="border-b border-blue-gray-100 bg-blue-gray-50 pl-4">
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  style={{ fontWeight: "bolder" }}
-                  className="font-normal leading-none opacity-70 text-center"
-                >
-                  Delivery Free
-                </Typography>
-              </th>
-              <th className="border-b border-blue-gray-100 bg-blue-gray-50 pl-4">
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  style={{ fontWeight: "bolder" }}
-                  className="font-normal leading-none opacity-70 text-center"
-                >
-                  Interest
-                </Typography>
-              </th>
-
-              <th className="border-b border-blue-gray-100 bg-blue-gray-50 pl-4">
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  style={{ fontWeight: "bolder" }}
-                  className="font-normal leading-none opacity-70 text-center"
-                >
-                  Update
-                </Typography>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {delivers.map((deliver) => (
-              <tr key={deliver._id}>
-                <td className="pl-3">
-                  <Input
-                    type="text"
-                    value={deliver.chargePrice}
-                    onChange={(e) =>
-                      handleInputChange(e, deliver._id, "chargePrice")
-                    }
-                    disabled={!deliver.editMode}
-                  />
-                </td>
-                <td className="pl-3">
-                  <Input
-                    type="text"
-                    value={deliver.deliveryFree}
-                    onChange={(e) =>
-                      handleInputChange(e, deliver._id, "deliveryFree")
-                    }
-                    disabled={!deliver.editMode}
-                  />
-                </td>
-                <td className="pl-3">
-                  <Input
-                    type="text"
-                    value={deliver.interest}
-                    onChange={(e) =>
-                      handleInputChange(e, deliver._id, "interest")
-                    }
-                    disabled={!deliver.editMode}
-                  />
-                </td>
-                <td className="pl-4">{renderSaveOrEditButton(deliver._id)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+      <div className="border border-black size-max mt-20 mb-20 ml-72 text-gray-600 bg-gray-200 rounded-lg">
+        <p className="m-3 font-light">
+          <ul>
+            <li>
+              The delivery cost calculation is based on the total quantity of
+              items being delivered.
+            </li>
+            <li>
+              If the total quantity is below a specified threshold for free
+              delivery:
+            </li>
+            <ul>
+              <li>
+                <b className="font-semibold">
+                  A Normal delivery charge is applied.
+                </b>
+              </li>
+            </ul>
+            <li>If the total quantity exceeds the free delivery threshold:</li>
+            <ul>
+              <li>
+                <b className="font-semibold">
+                  The Normal delivery charge is applied.
+                </b>
+              </li>
+              <li>
+                <b className="font-semibold">
+                  An additional charge, calculated as a percentage of the Normal
+                  charge, is added.
+                </b>
+              </li>
+            </ul>
+            <li>
+              The total delivery cost is the sum of the Normal charge and any
+              applicable additional charge.
+            </li>
+            <li>
+              <b className="font-semibold p-6 ">
+                Additional charge = Percentage * Delivery Free Threshold
+              </b>
+            </li>
+            <li>
+              <b className="font-semibold p-6">
+                Total delivery cost = Normal charge + Additional charge
+              </b>
+            </li>
+          </ul>
+        </p>
+      </div>
     </Card>
   );
 }
